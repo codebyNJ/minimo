feee5b64d135db7064f5ff991be30c812d71ad87
Nijeesh NJ <nijeesh10th@gmail.com>
2026-06-21 22:17:22 +0530

    feat: add yaml-based config package

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.0
 
 require (
 	github.com/fsnotify/fsnotify v1.10.1
+	gopkg.in/yaml.v3 v3.0.1
 	modernc.org/sqlite v1.53.0
 )
 
---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	DebounceMS       int      `yaml:"debounce_ms"`
+	PollIntervalSec  int      `yaml:"poll_interval_seconds"`
+	EnabledProviders []string `yaml:"enabled_providers"`
+}
+
+func Default() Config {
+	return Config{
+		DebounceMS:      500,
+		PollIntervalSec: 2,
+	}
+}
+
+func DefaultPath() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		home = ""
+	}
+	return filepath.Join(home, ".ctx", "config.yaml")
+}
+
+func Load(path string) (Config, error) {
+	cfg := Default()
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return cfg, nil
+		}
+		return cfg, err
+	}
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return cfg, err
+	}
+	return cfg, nil
+}
+
+func (c Config) Debounce() time.Duration {
+	return time.Duration(c.DebounceMS) * time.Millisecond
+}
+
+func (c Config) PollInterval() time.Duration {
+	return time.Duration(c.PollIntervalSec) * time.Second
+}
